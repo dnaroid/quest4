5fe1cfe0355e61481fe14f1650bba87e6773113c
buzz <buzz@localhost>
2016-02-03 17:04:16 +0300

    1

--- src/quest4/Quest4.java
+++ src/quest4/Quest4.java
@@ -7,10 +7,6 @@ import quest4.Aircraft.Aircraft;
  * @author buzz
  */
 public class Quest4 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Aircraft aircraft = new Aircraft(1);
         System.out.println(aircraft.toString());
